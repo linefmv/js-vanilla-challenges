f650840e5630d38d6127c43f9d4411674a43533e
Aline Viana <alinemaciel018@gmail.com>
2021-09-01 19:03:05 -0300

    doing decrease

---
+++ counter/app.js
@@ -0,0 +1,15 @@
+// set intial count 
+let count = 0;
+
+// select value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+        const styles = e.currentTarget.classList;
+        if (styles.contains('decrease')) {
+            count--;
+        }
+    })
+})
