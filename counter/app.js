7c827108d4efd2fe87677cfc8189a36520bb4254
Aline Viana <alinemaciel018@gmail.com>
2021-09-01 19:06:02 -0300

    doing increase and 0

--- counter/app.js
+++ counter/app.js
@@ -8,8 +8,13 @@ const btns = document.querySelectorAll(".btn");
 btns.forEach(function (btn) {
     btn.addEventListener('click', function (e) {
         const styles = e.currentTarget.classList;
-        if (styles.contains('decrease')) {
+        if (styles.contains("decrease")) {
             count--;
+        } else if (styles.contains("increase")) {
+            count++;
+        } else {
+            count = 0;
         }
-    })
-})
+        value.textContent = count;
+    });
+});
