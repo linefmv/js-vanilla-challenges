10321f664d56b89c9a9568905105cc25c2c8efe6
Aline Viana <alinemaciel018@gmail.com>
2021-09-13 17:07:16 -0300

    exerc

---
+++ dom-exercises/app.js
@@ -0,0 +1,28 @@
+// Select the section with an id of container without using querySelector.
+document.getElementById("container")
+
+// Select the section with an id of container using querySelector.
+document.querySelector("#container")
+
+// Select all of the list items with a class of "second".
+document.querySelectorAll(".second")
+// Select a list item with a class of third, but only the list item inside of the ol tag.
+document.getElementsByClassName("ol.third")
+
+// Give the section with an id of container the text "Hello!".
+sayOla = document.getElementById("container")[0];
+sayOla.innerHTML = "hellooooo"
+
+// Add the class main to the div with a class of footer.
+
+// Remove the class main on the div with a class of footer.
+
+// Create a new li element.
+
+// Give the li the text "four".
+
+// Append the li to the ul element.
+
+// Loop over all of the lis inside the ol tag and give them a background color of "green".
+
+// Remove the div with a class of footer.
